36a879b143162c770a5330c07e03248eca012c08
chloetohce <chloetohce@gmail.com>
2024-10-11 11:52:13 +0800

    csv analytics

--- src/stats/Analytics.java
+++ src/stats/Analytics.java
@@ -31,13 +31,13 @@ public class Analytics {
                 app.put(columns[i], appData[i].replace("\"", ""));
             }
 
-            storeData.put(appData[1], app);
+            storeData.put(appData[0].replace("\"", ""), app);
         }
         br.close();
     }
 
-    private static float validRating(String f) {
-        return f.equals("NaN") ? 0.0f : Float.parseFloat(f);
+    private static float validRating(Float f) {
+        return f.equals(Float.parseFloat("NaN")) ? 0.0f : f;
     }
 
     private Map<String, Map<String, Float>> sortCategory() {
@@ -48,7 +48,7 @@ public class Analytics {
             String appName = e.getKey();
             Map<String, String> data = e.getValue();
             String category = data.get("Category");
-            float rating = validRating(data.get("Rating"));
+            float rating = Float.parseFloat(data.get("Rating"));
 
             Map<String, Float> update = new HashMap<>();
             if (categories.containsKey(category)) {
@@ -60,8 +60,63 @@ public class Analytics {
         return categories;
     }
 
-    // public Map<String, String> getHighest() {
-    // }
+    public Map<String, String> getHighest() {
+        Map<String, Map<String, Float>> sortedByCat = this.sortCategory();
+        Set<String> categories = sortedByCat.keySet();
+        Map<String, String> highest = new HashMap<>();
+        for (String cat : categories) {
+            Map<String, Float> subCatApps = sortedByCat.get(cat);
+            highest.put(cat, "");
+            for (Map.Entry<String, Float> app : subCatApps.entrySet()) {
+                float currHighRating = subCatApps.getOrDefault(highest.get(cat), 0.0f);
+                String appName = app.getKey();
+                float appRating = app.getValue();
+                if (appRating > currHighRating) {
+                    highest.put(cat, appName);
+                }
+            }
+        }
+        return highest;
+    }
+
+    public Map<String, String> getLowest() {
+        Map<String, Map<String, Float>> sortedByCat = this.sortCategory();
+        Set<String> categories = sortedByCat.keySet();
+        Map<String, String> lowest = new HashMap<>();
+        for (String cat : categories) {
+            Map<String, Float> subCatApps = sortedByCat.get(cat);
+            lowest.put(cat, "");
+            for (Map.Entry<String, Float> app : subCatApps.entrySet()) {
+                float currHighRating = subCatApps.getOrDefault(lowest.get(cat), 5.0f);
+                String appName = app.getKey();
+                float appRating = app.getValue();
+                if (appRating < currHighRating) {
+                    lowest.put(cat, appName);
+                }
+            }
+        }
+        return lowest;
+    }
+
+    public Map<String, Float> getAverage() {
+        Map<String, Map<String, Float>> sortedByCat = this.sortCategory();
+        Set<String> categories = sortedByCat.keySet();
+        Map<String, Float> averages = new HashMap<>();
+
+        for (String cat : categories) {
+            Map<String, Float> subCatApps = sortedByCat.get(cat);
+            float avg = 0.0f;
+            int count = 0;
+
+            for (Float rating : subCatApps.values()) {
+                count++;
+                avg += validRating(rating); //Decided to count NaN values as 0, treated as unrated
+            }
+            avg = avg / count;
+            averages.put(cat, avg);
+        }
+        return averages;
+    }
 
     public static void main(String[] args) throws IOException {
         String f = "data" + File.separator + "googleplaystore.csv";
@@ -70,9 +125,24 @@ public class Analytics {
         }
         Analytics a = new Analytics();
         a.getData(new File(f));
-        Map<String, Float> test = a.sortCategory().get("ART_AND_DESIGN");
-        for (Map.Entry<String, Float> e : a.sortCategory().get("ART_AND_DESIGN").entrySet()) {
+        Map<String, String> highest = a.getHighest();
+        System.out.println("================= HIGHEST =================");
+        for (Map.Entry<String, String> e : highest.entrySet()) {
             System.out.println(e.getKey() + " : " + e.getValue());
         }
+        System.out.println();
+        Map<String, String> lowest = a.getLowest();
+        System.out.println("================= LOWEST =================");
+        for (Map.Entry<String, String> e : lowest.entrySet()) {
+            System.out.println(e.getKey() + " : " + e.getValue());
+        }
+        System.out.println();
+        Map<String, Float> averages = a.getAverage();
+        System.out.println("================= AVERAGES =================");
+        for (Map.Entry<String, Float> e : averages.entrySet()) {
+            System.out.println(e.getKey() + " : " + e.getValue());
+        }
+
+
     }
 }
